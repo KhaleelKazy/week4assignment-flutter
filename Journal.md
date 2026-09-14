e1a80620e59a99d859752202a6d2de15bd8c6a3f
KhaleelKazy <khaleelgoldsby@gmail.com>
2026-09-13 22:16:30 -0500

    commit 2
    phase 2

---
+++ Journal.md
@@ -0,0 +1,1 @@
+The most challenging part was getting the padding right
--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,35 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Image.asset('assets/batman.png'),
-          ],
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue[50],
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      'Khaleel',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Flutter Developer',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Row(
+                      children: [
+                        Text('Email: '),
+                        Text('KhaleelGoldsby@Gmail.com'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset('assets/batman.png'),
+            ],
+          ),
         ),
       ),
     ),
